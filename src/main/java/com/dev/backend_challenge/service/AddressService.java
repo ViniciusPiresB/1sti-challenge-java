cd54b512803f6740405390380812e6c950645dac
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-26 19:17:58 -0300

    feat: Update Address of user

--- src/main/java/com/dev/backend_challenge/service/AddressService.java
+++ src/main/java/com/dev/backend_challenge/service/AddressService.java
@@ -2,9 +2,11 @@ package com.dev.backend_challenge.service;
 
 import com.dev.backend_challenge.dto.Address.AddressCreateDTO;
 import com.dev.backend_challenge.dto.Address.AddressDTO;
+import com.dev.backend_challenge.dto.Address.AddressUpdateDTO;
 import com.dev.backend_challenge.entity.Address;
 import com.dev.backend_challenge.entity.User;
 import com.dev.backend_challenge.repository.AddressRepository;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,16 @@ import org.springframework.stereotype.Service;
 public class AddressService {
     private final AddressRepository addressRepository;
     private final ObjectMapper objectMapper;
+
+    public AddressDTO updateAddressOfUser(String userId, AddressUpdateDTO addressUpdateDTO) throws JsonMappingException {
+        Address address = this.addressRepository.findByUserId(userId);
+
+        this.objectMapper.updateValue(address, addressUpdateDTO);
+
+        Address updatedAddress = this.addressRepository.save(address);
+
+        return this.objectMapper.convertValue(updatedAddress, AddressDTO.class);
+    }
 
     public AddressDTO create(AddressCreateDTO addressCreateDTO, User user) {
         Address address = this.objectMapper.convertValue(addressCreateDTO, Address.class);
