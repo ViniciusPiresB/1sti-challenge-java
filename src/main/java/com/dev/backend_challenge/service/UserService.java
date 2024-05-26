97408b8822a783f0af383ae5f89e85302db652e0
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-26 18:18:41 -0300

    feat: Find all users

--- src/main/java/com/dev/backend_challenge/service/UserService.java
+++ src/main/java/com/dev/backend_challenge/service/UserService.java
@@ -32,4 +32,46 @@ public class UserService {
 
         return this.objectMapper.convertValue(user, UserDTO.class);
     }
+
+    public List<UserDTO> findAll(){
+        return this.userRepository.findAll().stream().map(user -> objectMapper.convertValue(user, UserDTO.class)).collect(Collectors.toList());
+    }
+
+    public UserDTO findOne(String cpf){
+        User user = this.getUser(cpf);
+
+        return this.objectMapper.convertValue(user, UserDTO.class);
+    }
+
+    public UserDTO update(UserUpdateDTO userUpdateDTO, String cpf) throws JsonMappingException {
+        User user = this.getUser(cpf);
+
+        this.objectMapper.updateValue(user, userUpdateDTO);
+
+        User updatedUser =  this.userRepository.save(user);
+
+        return this.objectMapper.convertValue(updatedUser, UserDTO.class);
+    }
+
+    public UserDTO delete(String cpf, String activeUserCpf) {
+        User user = this.getUser(cpf);
+
+        user.setStatus(Status.DELETED);
+        user.setDeletedBy(activeUserCpf);
+        user.setDeletedAt(LocalDate.now());
+
+        this.userRepository.save(user);
+
+        return this.objectMapper.convertValue(user, UserDTO.class);
+    }
+
+    private User getUser(String cpf){
+        User user = this.userRepository.findByCpf(cpf);
+
+        if(user == null) throw new ValidationException(ErrorEnum.NOT_FOUND);
+
+        if(user.getStatus() == Status.DELETED) throw new ValidationException((ErrorEnum.DELETED_ENTITY));
+
+        return user;
+    }
 }
