77a648a0199774181746dd54d39828d57c0903ef
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-26 15:26:15 -0300

    init: First commit

---
+++ src/main/java/com/dev/backend_challenge/BackendChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/backend_challenge/BackendChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
