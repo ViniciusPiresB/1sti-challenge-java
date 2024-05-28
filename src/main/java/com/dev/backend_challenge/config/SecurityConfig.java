1709052d3221fba27a79b0e85adb36af35477b92
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-05-28 18:13:30 -0300

    feat: Permit swagger routes

--- src/main/java/com/dev/backend_challenge/config/SecurityConfig.java
+++ src/main/java/com/dev/backend_challenge/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/user/{cpf}").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/user/address/{cpf}").hasRole("USER")
